958a36527369ae91e5ea901973b920467441b8b0
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-08-12 20:24:04 -0300

    Iniciando com padrão de projetos Strategy

---
+++ src/Impostos/Imposto.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Alura\DesignPattern\Impostos;
+
+use Alura\DesignPattern\Orcamento;
+
+interface Imposto {
+    public function calculaImposto(Orcamento $orcamento): float;
+}
---
+++ testes.php
@@ -0,0 +1,13 @@
+<?php
+require "vendor/autoload.php";
+
+use Alura\DesignPattern\CalculadoraDeImpostos;
+use Alura\DesignPattern\Impostos\Icms;
+use Alura\DesignPattern\Orcamento;
+
+$calculadora = new CalculadoraDeImpostos();
+
+$orcamento = new Orcamento();
+$orcamento->valor = 100;
+
+echo $calculadora->calcula($orcamento,  new Icms());
