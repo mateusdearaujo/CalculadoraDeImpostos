f6ce1434dda019214ca3b112d1a7db8f82452835
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-08-12 23:20:13 -0300

    Implementando padrão de projeto Chain of Responsability

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -1,9 +1,8 @@
 <?php
 
-
 namespace Alura\DesignPattern;
 
-
 class Orcamento {
+    public int $quantidadeItens;
     public float $valor;
 }
--- testes.php
+++ testes.php
@@ -1,13 +1,22 @@
 <?php
 require "vendor/autoload.php";
 
+use Alura\DesignPattern\CalculadoraDeDescontos;
 use Alura\DesignPattern\CalculadoraDeImpostos;
 use Alura\DesignPattern\Impostos\Icms;
 use Alura\DesignPattern\Orcamento;
+//$calculadora = new CalculadoraDeImpostos();
+//
+//$orcamento = new Orcamento();
+//$orcamento->valor = 100;
+//
+//echo $calculadora->calcula($orcamento,  new Icms());
 
-$calculadora = new CalculadoraDeImpostos();
+$calculadora = new CalculadoraDeDescontos();
 
 $orcamento = new Orcamento();
-$orcamento->valor = 100;
+$orcamento->valor = 1000;
+$orcamento->quantidadeItens = 4;
 
-echo $calculadora->calcula($orcamento,  new Icms());
+
+echo $calculadora->calculaDescontos($orcamento);
